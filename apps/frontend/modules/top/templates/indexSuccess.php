c0b45fc59a9b4431d3e20eab19aaadcbe20c4e08
Kaito <kaito.titech@gmail.com>
2014-10-03 22:58:16 +0900

    変更

--- apps/frontend/modules/top/templates/indexSuccess.php
+++ apps/frontend/modules/top/templates/indexSuccess.php
@@ -1,21 +1,22 @@
-<div id='top'>
-	<div>
-		<div id='top-logo'>
+<div class="top">
+	<div class="header">
+		<div class="top-logo">
 			<h2>PROGATE</h2>
 			<h1>BOOT CAMP</h1>
 		</div>
-		<div id='top-navi'>
-			<ul id='navi-item'>
+		<div class="top-navi">
+			<ul class="navi-item">
 				<li>what we learn</li>
 				<li>schedule</li>
 				<li>特徴</li>
 				<li>申し込み</li>
 		</div>
 	</div>
+	<div class="title">Learn to Code, Learn to be Creative</div>
 </div>
-<div id='about'></div>
+<div class="about"></div>
 
-<div></div>
-<div></div>
-<div></div>
-<div></div>
+<div class="learn"></div>
+<div class="schedule"></div>
+<div class="apply"></div>
+<div class="footer"></div>
