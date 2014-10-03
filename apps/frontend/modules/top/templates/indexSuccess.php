a4c3581a948efc4c88a2b36264dafb7f7b942c11
鈴木介翔 <kaito@suzukikaito-no-MacBook-Air.local>
2014-10-03 22:40:06 +0900

    最初のコミット

---
+++ apps/frontend/modules/top/templates/indexSuccess.php
@@ -0,0 +1,20 @@
+<div id='top'>
+	<div>
+		<div id='top-logo'>
+			<h2>PROGATE</h2>
+			<h1>BOOT CAMP</h1>
+		</div>
+		<div id='top-navi'>
+			<ul id='navi-item'>
+				<li>what we learn</li>
+				<li>schedule</li>
+				<li>特徴</li>
+				<li>申し込み</li>
+		</div>
+	</div>
+</div>
+<div id='about'></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
