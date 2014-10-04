e3310e61b1ab62ebd03c5ecfda17efbbabcb92e7
Kaito <kaito.titech@gmail.com>
2014-10-04 14:06:10 +0900

    info finish

--- apps/frontend/modules/top/templates/indexSuccess.php
+++ apps/frontend/modules/top/templates/indexSuccess.php
@@ -65,6 +65,45 @@
 		</div>	
 	</div>
 </div>
+<div class="info">
+	<div class="info-top">Camp #1 - 概要</div>
+	<div class="info-bottom">
+		<div class="info-main">
+			<div class="info-main-title">11/8~29 TOKYO</div>
+			<div class="info-main-list">
+				<div class="info-main-item">■定員：15名</div>
+				<div class="info-main-item">■日程：11/8~11/29  
+・出席必須日は毎週土曜日（11/8, 11/15, 11/22, 11/29）全４回  
+・日曜日は質問会（自由学習日）  
+・初日は懇親会、最終日は打ち上げを予定</div>
+				<div class="info-main-item">■時間：10:00~19:00</div>
+				<div class="info-main-item">■場所：株式会社Bitcellar本社オフィス  
+麻布十番駅徒歩5分</div>
+				<div class="info-main-item">■参加費：¥20,000/人</div>
+				<div class="info-main-item">■参加条件：①パソコン持参、②所定の事前課題の完了（後述）、③全４回の出席必須日に出席できること</div>
+				<div class="info-main-item">■事前課題：①②のいずれか  
+①Prog-8.comのHTML, PHPの課題  
+②HTML, PHP経験者は実績を証明できるものを提出。  
+（事前課題は申し込みの段階で完了されている必要はありません。）</div>
+			</div>
+		</div>
+		<div class="info-company">
+			<div class="info-company-title">Bitcellar 協賛</div>
+			<div class="info-company-list">
+				<div class="info-company-item">
+					<div class="info-company-text">「Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. 」  
+秋山 博紀 / UIUX engineer</div>
+					<div class="info-company-image"></div>
+				</div>
+				<div class="info-company-item">
+					<div class="info-company-text">「Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. 」  
+川村　亮介 / 代表取締役社長</div>
+					<div class="info-company-image"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 <div class="schedule"></div>
 <div class="apply"></div>
 <div class="footer"></div>
