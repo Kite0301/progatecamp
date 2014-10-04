0fe3dee99c380257109031e044b5f1c9e5bb4fb0
Kaito <kaito.titech@gmail.com>
2014-10-04 13:38:54 +0900

    about finish

--- apps/frontend/modules/top/templates/indexSuccess.php
+++ apps/frontend/modules/top/templates/indexSuccess.php
@@ -24,7 +24,13 @@
 		</ul>
 	</div>
 </div>
-<div class="about"></div>
+<div class="about">
+	<div class="about-top">P-Campとは</div>
+	<div class="about-bottom">
+		<div class="about-image"></div>
+		<div class="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.</div>
+	</div>
+</div>
 
 <div class="learn"></div>
 <div class="schedule"></div>
